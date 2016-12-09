d9d632cdd76f1e276f94f499f0d6eaf04ebe4695
vinfrovenator <guyader.v@gmail.com>
2016-12-09 15:05:41 +0100

    Activity vide qui contiendra le formulaire de choix des magasin dnt on veut affiché les résultats

---
+++ app/src/main/java/org/mydarties/resp/Consult_form.java
@@ -0,0 +1,22 @@
+package org.mydarties.resp;
+
+import android.os.Bundle;
+
+import org.mydarties.R;
+import org.mydarties.drawer.BaseActivity;
+
+/**
+ * Created by DartiesB on 09/12/2016.
+ */
+
+public class Consult_form extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        onCreateDrawer(R.layout.activity_form_consult_resp);
+
+
+    }
+
+}
