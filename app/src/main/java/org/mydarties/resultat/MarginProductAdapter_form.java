a11cd0ead5b0c65e5b2d1fa198790fc3b74cf788
vinfrovenator <guyader.v@gmail.com>
2017-01-11 15:50:50 +0100

    Modif de editText -> TextView marge

--- app/src/main/java/org/mydarties/resultat/MarginProductAdapter_form.java
+++ app/src/main/java/org/mydarties/resultat/MarginProductAdapter_form.java
@@ -5,7 +5,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
-import android.widget.EditText;
+import android.widget.TextView;
 
 import org.mydarties.R;
 
@@ -31,9 +31,9 @@ public class MarginProductAdapter_form extends ArrayAdapter<Product>{
         SalesProductViewHolder viewHolder = (SalesProductViewHolder) convertView.getTag();
         if(viewHolder == null){
             viewHolder = new SalesProductViewHolder();
-            viewHolder.name = (EditText) convertView.findViewById(R.id.nameProduitEdit);
-            viewHolder.objMargin =(EditText) convertView.findViewById(R.id.marginObjEdit);
-            viewHolder.realMargin = (EditText) convertView.findViewById(R.id.marginRealEdit);
+            viewHolder.name = (TextView) convertView.findViewById(R.id.nameProduitEdit);
+            viewHolder.objMargin =(TextView) convertView.findViewById(R.id.marginObjEdit);
+            viewHolder.realMargin = (TextView) convertView.findViewById(R.id.marginRealEdit);
 
             convertView.setTag(viewHolder);
         }
@@ -43,12 +43,14 @@ public class MarginProductAdapter_form extends ArrayAdapter<Product>{
 
         //remplissage des vues
         viewHolder.name.setText(""+product.getName());
+        viewHolder.objMargin.setText(""+product.getObjMargin());
+        viewHolder.realMargin.setText(""+product.getRealMargin());
         return convertView;
     }
 
     private class SalesProductViewHolder{
-        public EditText name;
-        public EditText realMargin;
-        public EditText objMargin;
+        public TextView name;
+        public TextView realMargin;
+        public TextView objMargin;
     }
 }
