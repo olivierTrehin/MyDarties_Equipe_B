ef6f165aba692ba9c25bf02dba70431115548902
vinfrovenator <guyader.v@gmail.com>
2017-01-11 15:51:02 +0100

    Modif de editText -> TextView ventes

--- app/src/main/java/org/mydarties/resultat/SalesProductAdapter_form.java
+++ app/src/main/java/org/mydarties/resultat/SalesProductAdapter_form.java
@@ -5,7 +5,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
-import android.widget.EditText;
+import android.widget.TextView;
 
 import org.mydarties.R;
 
@@ -31,9 +31,9 @@ public class SalesProductAdapter_form extends ArrayAdapter<Product>{
         SalesProductViewHolder viewHolder = (SalesProductViewHolder) convertView.getTag();
         if(viewHolder == null){
             viewHolder = new SalesProductViewHolder();
-            viewHolder.name = (EditText) convertView.findViewById(R.id.nameProduitEdit);
-            viewHolder.objSales =(EditText) convertView.findViewById(R.id.salesObjEdit);
-            viewHolder.realSales = (EditText) convertView.findViewById(R.id.salesRealEdit);
+            viewHolder.name = (TextView) convertView.findViewById(R.id.nameProduitEdit);
+            viewHolder.objSales =(TextView) convertView.findViewById(R.id.salesObjEdit);
+            viewHolder.realSales = (TextView) convertView.findViewById(R.id.salesRealEdit);
 
             convertView.setTag(viewHolder);
         }
@@ -43,12 +43,14 @@ public class SalesProductAdapter_form extends ArrayAdapter<Product>{
 
         //remplissage des vues
         viewHolder.name.setText(""+product.getName());
+        viewHolder.objSales.setText(""+product.getObjSales());
+        viewHolder.realSales.setText(""+product.getRealSales());
         return convertView;
     }
 
     private class SalesProductViewHolder{
-        public EditText name;
-        public EditText realSales;
-        public EditText objSales;
+        public TextView name;
+        public TextView realSales;
+        public TextView objSales;
     }
 }
